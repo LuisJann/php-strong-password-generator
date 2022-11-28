3e9275ef612f99738b5c9bb26e216cdd6f7f66e7
LuisJann <giannoneluigi10@gmail.com>
2022-11-28 16:26:43 +0100

    password generator (milestone 1)

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
-echo "ciao";
+$password_length = $_GET["password"] ?? "";
+
+
+function randomPassword($password_length)
+{
+    $charapter = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    $charapterLength = strlen($charapter) - 1;
+    for ($i = 0; $i < $password_length; $i++) {
+        $n = rand(0, $charapterLength);
+        $pass[] = $charapter[$n];
+    }
+    return implode($pass);
+}
+
+if ($password_length === "") {
+    $password_generated = "inserisci la lunghezza della password";
+} else {
+    $password_generated = randomPassword($password_length);
+}
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +32,28 @@ echo "ciao";
 </head>
 
 <body>
-
+    <div class="container d-flex flex-column justify-content-center bg-warning p-5">
+        <h1 class="light">Generatore di Password</h1>
+        <p></p>
+        <form action="index.php" method="GET">
+            <label for="password">
+                <span class="ps-4">Lunghezza della password</span>
+                <input type="text" id="password" name="password">
+                <button class="btn btn-primary" type="submit">Invia</button>
+                <button class="btn btn-danger" type="reset">Resetta</button>
+            </label>
+        </form>
+        <h3>la tua password è: <?php echo $password_generated ?></h3>
+        <div class="alert <?php echo $password_length !== "" ? "alert-success" : "alert-danger" ?>">
+            <?php if ($password_length === "") {
+                echo "inserisci la lunghezza della password";
+            } else {
+                $password_generated = randomPassword($password_length);
+                echo "ecco la tua password";
+            }
+            ?>
+        </div>
+    </div>
 </body>
 
 </html>
